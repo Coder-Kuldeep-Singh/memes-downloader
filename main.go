c8ac47d0a120c8b4dc02e295b90f7d1fea2f6dec
mavensingh <kuldeep.avsar@gmail.com>
2020-07-06 18:42:29 +0530

    created meme downlader program

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -36,7 +38,7 @@ func ResponseURL(url string) (*http.Response, error) {
 		return nil, err
 	}
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
-	time.Sleep(time.Second * 5)
+	// time.Sleep(time.Second * 60)
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
@@ -72,6 +74,42 @@ func MEMEPAGE(response *http.Response) ([]MEMES, error) {
 	return results, err
 }
 
+func DownloadMEME(response *http.Response) ([]byte, error) {
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+
+		// panic(err)
+		return body, err
+	}
+	return body, err
+}
+
+func CreateFolder() {
+	_, err := os.Stat("memes")
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll("memes", 0755)
+		if errDir != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func GenerateFile(filename string) (*os.File, error) {
+	create, err := os.Create("./memes/" + filename + ".jpg")
+	if err != nil {
+		return nil, err
+	}
+	return create, nil
+}
+
+func AppendData(file *os.File, data []byte) (string, error) {
+	size, err := file.WriteString(string(data))
+	if err != nil {
+		return "", err
+	}
+	return string(size), nil
+}
+
 func Combination(url string) []MEMES {
 	resp, err := ResponseURL(url)
 	if err != nil {
@@ -79,7 +117,7 @@ func Combination(url string) []MEMES {
 		log.Println(err.Error())
 		return nil
 	}
-	time.Sleep(time.Second * 5)
+	// time.Sleep(time.Second * 5)
 	body, err := MEMEPAGE(resp)
 	if err != nil {
 		log.Println("error to take page response maybe data is missing ", err)
@@ -89,9 +127,43 @@ func Combination(url string) []MEMES {
 	return body
 }
 
+func SaveIntoSystem(url string) {
+	body := Combination(url)
+	// fmt.Printf("%v\n", body)
+	CreateFolder()
+	for _, resp := range body {
+		data, err := ResponseURL(resp.ImageUrl)
+		if err != nil {
+			log.Println("error to take response of the page ", err)
+			log.Println(err.Error())
+		}
+		body, err := DownloadMEME(data)
+		if err != nil {
+			log.Println("Error to get Image response ", body)
+			log.Println(err)
+			log.Println(err.Error())
+		}
+		filename := strings.Replace(resp.ImageUrl, "/", "", -1)
+		file, err := GenerateFile(filename)
+		if err != nil {
+			log.Printf("Error to create File %v\n", filename)
+			log.Println(err)
+			log.Println(err.Error())
+		}
+		_, err = AppendData(file, body)
+		if err != nil {
+			log.Println("Error to Append data into File ", filename)
+			log.Println(err)
+			log.Println(err.Error())
+		}
+		fmt.Printf("%v\n", filename)
+
+	}
+	fmt.Println("Memes Downloaded Successfully")
+
+}
+
 func main() {
 	url := "https://www.reddit.com/r/dankmemes/"
-	body := Combination(url)
-	fmt.Printf("%v\n", body)
-
+	SaveIntoSystem(url)
 }
